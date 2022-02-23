e2554fb5d93feec6afd5a03a552f06e4b87ca5fd
ValentinLAPLAUD <vallap.biophysics@protonmail.com>
2022-02-23 15:31:02 +0100

    Externalized experiment list

---
+++ ExperimentList.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 23 2022
+
+@author: Valentin Laplaud
+
+List of experiments
+
+"""
+
+Scale210903 = 1.94 # Spatial scale (µm/px) for 16X 03-09-2021
+FPH210903 = 2 # Frames per hour
+Delay210903 = 30 # Delay between imbibition and experiment start
+
+Scale210914 = 1.24 # Spatial scale (µm/px) for 25X 14-09-2021
+FPH210914 = 2 # Frames per hour
+Delay210914 = 30 # Delay between imbibition and experiment start
+
+Scale210927 = 1.24 # Spatial scale (µm/px) for 25X 27-09-2021
+FPH210927 = 2 # Frames per hour
+Delay210927 = 30 # Delay between imbibition and experiment start
+
+Scale211022 = 0.97 # Spatial scale (µm/px) for 32X 27-09-2021
+FPH211022 = 2 # Frames per hour
+Delay211022 = 30 # Delay between imbibition and experiment start
+
+Scale211105 = 1.55 # Spatial scale (µm/px) for 20X 05-11-2021
+FPH211105 = 2 # Frames per hour
+Delay211105 = 30 # Delay between imbibition and experiment start
+
+Scale211222 = 1.55 # Spatial scale (µm/px) for 20X 22-12-2021
+FPH211222 = 2 # Frames per hour
+Delay211222 = 35 # Delay between imbibition and experiment start
+
+Scale220107 = 1.94 # Spatial scale (µm/px) for 16X 07-01-2022
+FPH220107 = 2 # Frames per hour
+Delay220107 = 55 # Delay between imbibition and experiment start
+
+Scale220112 = 1.94 # Spatial scale (µm/px) for 16X 12-01-2022
+FPH220112 = 2 # Frames per hour
+Delay220112 = 45 # Delay between imbibition and experiment start
+
+Scale220114 = 1.94 # Spatial scale (µm/px) for 16X 14-01-2022
+FPH220114 = 2 # Frames per hour
+Delay220114 = 50 # Delay between imbibition and experiment start
+
+Scale220124 = 1.94 # Spatial scale (µm/px) for 16X 24-01-2022
+FPH220124 = 2 # Frames per hour
+Delay220124 = 40 # Delay between imbibition and experiment start
+
+Scale220203 = 1.94 # Spatial scale (µm/px) for 16X 03-02-2022
+FPH220203 = 2 # Frames per hour
+Delay220203 = 30 # Delay between imbibition and experiment start
+
+Scale220214 = 1.94 # Spatial scale (µm/px) for 16X 14-02-2022
+FPH220214 = 2 # Frames per hour
+Delay220214 = 30 # Delay between imbibition and experiment start
+
+# Paths to data
+P210903_1 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\210903_V5_TestFlux\1mlh'
+P210903_5 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\210903_V5_TestFlux\500ulh'
+P210914_Ct = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\210914_V5_Manitol125mM\Ctrl'
+P210914_M125 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\210914_V5_Manitol125mM\Manitol125'
+P210927_1 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\210927_V5_TestFlux\1mlh'
+P210927_5 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\210927_V5_TestFlux\500ulh'
+P211022_Ct1 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\211022_DV6_Ctrls\Ctrl1'
+P211022_Ct2 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\211022_DV6_Ctrls\Ctrl2'
+P211105_Deg_Bsa = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\211105_DV6_Degas_Ctrls\BSA'
+P211105_Degas = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\211105_DV6_Degas_Ctrls\NoCoat'
+P211222_Deg_Bsa = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\211222_DV6_Degas_Ctrls\BSA'
+P211222_Degas = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\211222_DV6_Degas_Ctrls\NoCoat'
+P220107_S3 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220107_DV6_Stade1&3\Stade3'
+P220107_S1 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220107_DV6_Stade1&3\Stade1'
+P220112_S3 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220112_DV6_Stade1&3\Stade3'
+P220112_S1 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220112_DV6_Stade1&3\Stade1'
+P220114_S3 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220114_DV6_Stade1&3\Stade3'
+P220114_S1 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220114_DV6_Stade1&3\Stade1'
+P220124_Ct1 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220124_DV6_Ctrls\Ctrl1'
+P220124_Ct2 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220124_DV6_Ctrls\Ctrl2'
+P220203_Ct1 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220203_DV6_Ctrls\Ctrl1'
+P220203_Ct2 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220203_DV6_Ctrls\Ctrl2'
+P220214_Ct1 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220214_DV6_Ctrls\Ctrl1'
+P220214_Ct2 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220214_DV6_Ctrls\Ctrl2'
+
+
+
+# experiments of 03-09-2021, Puce V5, Ctrl with 1ml/heure medium flow
+StackList210903_1 = ['PPG1','PPG2','PPG3','PPG6','PPG7','PPG10','PPG11','PPG12','PPG13','PPG14','PPG17','PPG19',
+                   'PPG21','PPG22'] # 'PPG4','PPG5','PPG8','PPG9','PPG15','PPG16','PPG18','PPG20'
+
+# experiments of 03-09-2021, Puce V5, Ctrl with 500ul/heure medium flow
+StackList210903_5 = ['PPG3','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG11'] # 'PPG1','PPG2','PPG4'
+
+# experiments of 14-09-2021, Puce V5, Ctrl with 1ml/heure medium flow
+StackList210914_Ct = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG9','PPG10','PPG11'] # 'PPG8','PPG12'
+
+# experiments of 14-09-2021, Puce V5, 125mM manitol with 1ml/heure medium flow
+StackList210914_M125 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG7','PPG8','PPG9'] # 'PPG6'
+
+
+# experiments of 27-09-2021, Puce V5, Ctrl with 1ml/heure medium flow
+StackList210927_1 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG7','PPG10',
+                   'PPG11','PPG12','PPG13','PPG14','PPG15',] # 'PPG6','PPG8','PPG9'
+
+# experiments of 27-09-2021, Puce V5, Ctrl with 500ul/heure medium flow
+StackList210927_5 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG8','PPG9','PPG10','PPG11','PPG13'] #'PPG7','PPG12'
+
+
+# experiments of 22-10-2021, double puce V6, Ctrl 1 with 500ul/heure medium flow
+StackList211022_Ct1 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9',
+                   'PPG11','PPG12','PPG13','PPG14','PPG15','PPG16','PPG17','PPG18','PPG19',
+                       'PPG20','PPG21','PPG22','PPG23','PPG24','PPG25'] 
+                    # 'PPG10', 
+    
+# experiments of 22-10-2021, double puce V6, Ctrl 2 with 500ul/heure medium flow
+StackList211022_Ct2 = ['PPG1','PPG2','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10',
+                   'PPG11','PPG12','PPG13','PPG15','PPG16','PPG18','PPG19','PPG20',
+                   'PPG22','PPG23','PPG24','PPG26','PPG27','PPG28','PPG29']
+                    # 'PPG3',  'PPG14', 'PPG17', 'PPG21', 'PPG25',
+
+
+# experiments of 05-11-2021, double puce V6, Degased medium, BSA coated channel with 500ul/heure medium flow
+StackList211105_Deg_Bsa = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG9','PPG10',
+                     'PPG11', 'PPG13','PPG16','PPG18','PPG19','PPG20',
+                   'PPG21','PPG23','PPG24','PPG25','PPG26','PPG27','PPG28','PPG29','PPG30']  #  
+                       # PPG12, PPG8 dead , PPG15, PPG17 dead notch, PPG22 dead parts, PPG14 folded lost attach
+
+# experiments of 05-11-2021, double puce V6, Degased medium  with 500ul/heure medium flow
+StackList211105_Degas = ['PPG1','PPG2','PPG3','PPG4','PPG6','PPG7','PPG8','PPG9','PPG10',
+                   'PPG11','PPG12','PPG13','PPG14','PPG16','PPG17','PPG18','PPG19','PPG20'] # 
+                # PPG3 dead attach, PPG5 dead notch, 'PPG15' dead attach
+
+    
+# experiments of 22-12-2021, double puce V6, degased medium, bsa coating with 500ul/heure medium flow
+# /!\ /!\ Start 30 minutes later than usual, first image removed
+StackList211222_Deg_Bsa = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10',
+                   'PPG11','PPG12','PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20',
+                   'PPG21','PPG22','PPG23','PPG24','PPG25','PPG26','PPG28','PPG29','PPG30',
+                          'PPG31'] # PPG22 ,'PPG13' touche le bord de l'image # ,'PPG27' bad contouring
+
+
+# experiments of 22-12-2021, double puce V6, degased medium, no coating with 500ul/heure medium flow
+# /!\ /!\ Start 30 minutes later than usual, first image removed
+StackList211222_Degas = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG9','PPG10',
+                   'PPG11','PPG12','PPG13','PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20',
+                   'PPG21','PPG22','PPG23','PPG24','PPG25','PPG26','PPG27','PPG28','PPG29','PPG30']
+                    # PPG8 trop plié
+
+
+# experiments of 07-01-2022, double puce V6, degased medium, no coating with 500ul/heure medium flow
+# Stade1 (close to the center) cups
+StackList220107_S1 = ['PPG1','PPG3','PPG6','PPG9',
+                   'PPG11','PPG12','PPG13','PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20',
+                   'PPG21','PPG23','PPG24','PPG25','PPG27']
+                    # ,'PPG2','PPG7' contact avec autre PPG
+                    # ,'PPG5' = PPG3, 'PPG10' same as PPG9 ,'PPG26' same as 25
+                    # ,'PPG22' morte
+
+# experiments of 07-01-2022, double puce V6, degased medium, no coating with 500ul/heure medium flow
+# Stade3 (close to the edge) cups
+StackList220107_S3 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10',
+                   'PPG11','PPG12','PPG13','PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20']
+
+# experiments of 12-01-2022, double puce V6, degased medium, no coating with 500ul/heure medium flow
+# Stade1 (close to the center) cups
+StackList220112_S1 = ['PPG1','PPG2','PPG3','PPG5','PPG6','PPG8','PPG9','PPG10','PPG11','PPG13',
+                      'PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20','PPG21','PPG22','PPG23','PPG24','PPG25',
+                      'PPG26','PPG27','PPG28','PPG29','PPG31','PPG32','PPG33','PPG34','PPG35','PPG36','PPG37',
+                      'PPG38','PPG39','PPG40','PPG41','PPG42','PPG43','PPG44','PPG45','PPG46','PPG47','PPG48','PPG49',
+                      'PPG50','PPG51','PPG52','PPG53','PPG54','PPG55','PPG56','PPG57','PPG58','PPG59','PPG60','PPG61',
+                      'PPG62','PPG63','PPG64','PPG65','PPG66','PPG67','PPG68']
+                    # PPG30 erreur de création de la stack #,'PPG7' ppg hors champ image 0
+                    # ,'PPG4' same than 1,  'PPG12', same than 8
+
+# experiments of 12-01-2022, double puce V6, degased medium, no coating with 500ul/heure medium flow
+# Stade3 (close to the edge) cups
+StackList220112_S3 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG11','PPG12','PPG13',
+                      'PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20','PPG21','PPG22','PPG23','PPG24','PPG25',
+                      'PPG26','PPG27','PPG28','PPG29','PPG30','PPG31','PPG32','PPG33','PPG34','PPG35','PPG36','PPG37',
+                      'PPG38','PPG39','PPG40','PPG41','PPG42','PPG43','PPG44','PPG45','PPG46','PPG47','PPG48','PPG49',
+                      'PPG50']
+                        
+
+# experiments of 14-01-2022, double puce V6, degased medium, no coating with 500ul/heure medium flow
+# Stade1 (close to the center) cups
+StackList220114_S1 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG11','PPG12','PPG13',
+                      'PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20','PPG21','PPG22','PPG23','PPG24','PPG25',
+                      'PPG26','PPG27','PPG28','PPG29','PPG30','PPG31','PPG32','PPG33','PPG34','PPG35','PPG36','PPG37',
+                      'PPG38','PPG39','PPG40','PPG41','PPG42','PPG43','PPG44','PPG45','PPG46','PPG47','PPG48','PPG49',
+                      'PPG50','PPG51']
+                
+# experiments of 14-01-2022, double puce V6, degased medium, no coating with 500ul/heure medium flow
+# Stade3 (close to the edge) cups
+StackList220114_S3 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG11','PPG12','PPG13',
+                      'PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20','PPG21','PPG22','PPG23','PPG24','PPG25',
+                      'PPG26','PPG27','PPG28','PPG29','PPG30','PPG31'] # ,'PPG3233'
+                        
+
+# experiments of 24-01-2022, double puce V6, degased medium, no coating with 500ul/heure medium 
+# flow, chip ctrl 1
+StackList220124_Ct1 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG11','PPG12','PPG13',
+                      'PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20','PPG21','PPG22','PPG23','PPG24','PPG25',
+                      'PPG26','PPG27','PPG28','PPG29','PPG30','PPG31','PPG32','PPG33','PPG34','PPG35']
+                
+# experiments of 24-01-2022, double puce V6, degased medium, no coating with 500ul/heure medium 
+# flow, chip ctrl 2
+StackList220124_Ct2 = ['PPG1','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG11','PPG12','PPG13',
+                      'PPG14','PPG16','PPG17','PPG18','PPG19','PPG20','PPG21','PPG22','PPG23','PPG24','PPG25',
+                      'PPG26','PPG27','PPG28','PPG29','PPG30','PPG31','PPG32','PPG33','PPG34','PPG35','PPG36','PPG37',
+                      'PPG38','PPG39','PPG40','PPG41','PPG42','PPG43','PPG44','PPG45','PPG46','PPG47','PPG48','PPG49',
+                      'PPG50','PPG51','PPG52','PPG53'] # 'PPG2', poussiere qui touche, 'PPG15', partiellement morte
+
+
+# experiments of 03-02-2022, double puce V6, degased medium, no coating with 500ul/heure medium 
+# flow, chip ctrl 1
+StackList220203_Ct1 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG11','PPG12','PPG13',
+                      'PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20','PPG21','PPG22','PPG23','PPG24','PPG25',
+                      'PPG26','PPG27','PPG28','PPG29','PPG30','PPG31','PPG32','PPG33','PPG34','PPG35']
+                
+# experiments of 03-02-2022, double puce V6, degased medium, no coating with 500ul/heure medium 
+# flow, chip ctrl 2
+StackList220203_Ct2 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG11','PPG12','PPG13',
+                      'PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20','PPG21','PPG22','PPG23','PPG24','PPG25',
+                      'PPG26','PPG27','PPG28','PPG29','PPG30','PPG31','PPG32','PPG33','PPG34','PPG35','PPG36','PPG37',
+                      'PPG38','PPG39','PPG40','PPG41','PPG42','PPG43','PPG44','PPG45','PPG46','PPG47','PPG48','PPG49',
+                      'PPG50','PPG51','PPG52','PPG53']
+
+
+# experiments of 14-02-2022, double puce V6, degased medium, no coating with 500ul/heure medium 
+# flow, chip ctrl 1
+StackList220214_Ct1 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG11','PPG12','PPG13',
+                      'PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20','PPG21','PPG22','PPG23','PPG24','PPG25',
+                      'PPG26','PPG27','PPG28','PPG29','PPG30','PPG31','PPG32','PPG33','PPG34','PPG35']
+                
+# experiments of 14-02-2022, double puce V6, degased medium, no coating with 500ul/heure medium 
+# flow, chip ctrl 2
+StackList220214_Ct2 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG11','PPG12','PPG13',
+                      'PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20','PPG21','PPG22','PPG23','PPG24','PPG25',
+                      'PPG26','PPG27','PPG28','PPG29','PPG30','PPG31','PPG32','PPG33','PPG34','PPG35','PPG36','PPG37',
+                      'PPG38','PPG39','PPG40','PPG41','PPG42','PPG43','PPG44','PPG45','PPG46','PPG47','PPG48','PPG49',
+                      'PPG50','PPG51','PPG52','PPG53']
+
+print('Data choice made.')
