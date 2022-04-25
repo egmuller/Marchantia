f3fa2551af9123a9c584c18513560a668b1df97f
ValentinLAPLAUD <vallap.biophysics@protonmail.com>
2022-04-25 18:10:31 +0200

    Added Ctrl1 from 05/04/2022
    + minor changes (K -> E, L- > L/H, graphs...)

--- ExperimentList.py
+++ ExperimentList.py
@@ -59,13 +59,20 @@ Delay220214 = 30 # Delay between imbibition and experiment start
 Scale220214_Osmo = 1.94 # Spatial scale (µm/px) for 16X 14-02-2022
 FPH220214_Osmo = 120 # Frames per hour
 
-
 Scale220301 = 1.94 # Spatial scale (µm/px) for 16X 14-02-2022
 FPH220301 = 2 # Frames per hour
-Delay220301 = 38 # Delay between imbibition and experiment start
+Delay220301 = 37 # Delay between imbibition and experiment start
 
 Scale220301_Osmo = 1.94 # Spatial scale (µm/px) for 16X 14-02-2022
 FPH220301_Osmo = 120 # Frames per hour
+
+Scale220405 = 1.94 # Spatial scale (µm/px) for 16X 14-02-2022
+FPH220405 = 2 # Frames per hour
+Delay220405 = 30 # Delay between imbibition and experiment start
+
+Scale220405_Osmo = 1.94 # Spatial scale (µm/px) for 16X 14-02-2022
+FPH220405_Osmo = 120 # Frames per hour
+
 
 # Paths to data
 P210903_1 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\210903_V5_TestFlux\1mlh'
@@ -98,6 +105,10 @@ P220301_Ct = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220301_DV6_Manitol
 P220301_M100 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220301_DV6_Manitol100mM+OC\TimeEvolution\Manitol100mM'
 P220301_Ct_Osmo = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220301_DV6_Manitol100mM+OC\OsmoticChocs\Ctrl'
 P220301_M100_Osmo = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220301_DV6_Manitol100mM+OC\OsmoticChocs\Manitol100mM'
+P220405_Ct1 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220405_DV6_Ctrls+OC\TimeEvolution\Ctrl1'
+P220405_Ct2 = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220405_DV6_Ctrls+OC\TimeEvolution\Ctrl2'
+P220405_Ct1_Osmo = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220405_DV6_Ctrls+OC\OsmoticChoc\Ctrl1'
+P220405_Ct2_Osmo = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu\220405_DV6_Ctrls+OC\OsmoticChoc\Ctrl2'
 
 PFig = r'D:\Users\laplaud\Desktop\PostDoc\Data\Microflu'
 
@@ -260,7 +271,7 @@ StackList220214_Ct2 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','
 # experiments of 14-02-2022, double puce V6, degased medium, Osmotic choc after time evolution chip ctrl 1
 StackList220214_Ct1_Osmo = ['PPG1','PPG2','PPG3','PPG4','PPG7','PPG9','PPG10','PPG11','PPG12','PPG14','PPG18','PPG20','PPG21','PPG22',
                             'PPG23','PPG24','PPG26','PPG28','PPG29','PPG31','PPG32','PPG33','PPG35','PPG36','PPG37','PPG38','PPG39',
-                            'PPG42','PPG43','PPG44','PPG45','PPG46','PPG47','PPG48','PPG51','PPG52','PPG53','PPG55']
+                            'PPG42','PPG43','PPG44','PPG45','PPG46','PPG47','PPG48','PPG52','PPG53','PPG55']
                 
 # experiments of 14-02-2022, double puce V6, degased medium, Osmotic choc after time evolution chip ctrl 2
 StackList220214_Ct2_Osmo = ['PPG1','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG11','PPG12','PPG13','PPG14','PPG19','PPG20','PPG22',
@@ -292,5 +303,30 @@ StackList220301_Ct_Osmo = ['PPG1','PPG4','PPG5','PPG6','PPG7','PPG8','PPG11','PP
 # experiments of 14-02-2022, double puce V6, degased medium, Osmotic choc after time evolution chip ctrl 2
 StackList220301_M100_Osmo = ['PPG1','PPG7','PPG8','PPG10','PPG11','PPG26','PPG27','PPG28','PPG29','PPG30','PPG31','PPG34','PPG37',
                       'PPG38','PPG40','PPG41','PPG42','PPG43','PPG44','PPG45','PPG47','PPG49','PPG50','PPGA','PPGB','PPGC','PPGD','PPGE','PPGF']
+
+
+
+# experiments of 05-04-2022, double puce V6, degased medium, no coating with 500ul/heure medium 
+# flow, chip ctrl 1
+StackList220405_Ct1 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG11','PPG12','PPG13',
+                      'PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20','PPG21','PPG22','PPG23','PPG24','PPG25',
+                      'PPG26','PPG27','PPG28','PPG29','PPG30','PPG31','PPG32','PPG33','PPG34','PPG35']
+                
+# experiments of 05-04-2022, double puce V6, degased medium, no coating with 500ul/heure medium 
+# flow, chip ctrl 2
+##StackList220405_Ct2 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG11','PPG12','PPG13',
+  #                    'PPG14','PPG15','PPG16','PPG17','PPG18','PPG19','PPG20','PPG21','PPG22','PPG23','PPG24','PPG25',
+   #                   'PPG26','PPG27','PPG28','PPG29','PPG30','PPG31','PPG32','PPG33','PPG34','PPG35','PPG36','PPG37',
+    #                  'PPG38','PPG39','PPG40','PPG41','PPG42','PPG43','PPG44']
+
+# experiments of 05-04-2022, double puce V6, degased medium, Osmotic choc after time evolution chip ctrl 1
+StackList220405_Ct1_Osmo = ['PPG1','PPG5','PPG6','PPG7','PPG13','PPG15','PPG16','PPG17','PPG18','PPG21','PPG22','PPG25',
+                      'PPG28','PPG29','PPG30','PPG33','PPG35','PPGA','PPGB','PPGC','PPGD','PPGE','PPGF','PPGG']
+                
+# experiments of 05-04-2022, double puce V6, degased medium, Osmotic choc after time evolution chip ctrl 2
+#StackList220405_Ct2_Osmo = ['PPG1','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG11','PPG12','PPG13','PPG14','PPG19','PPG20','PPG22',
+ #                           'PPG23','PPG26','PPG27','PPG28','PPG29','PPG30','PPG31','PPG32','PPG33','PPG34','PPG35','PPG36','PPG37',
+  #                          'PPG38','PPG39','PPG40','PPG42','PPG43','PPG44','PPG45','PPG46','PPG47','PPG48','PPG49','PPG50','PPG51','PPG52',
+   #                         'PPG53','PPG54','PPG55','PPG56','PPG57','PPG58','PPG59','PPG60','PPG61','PPG62']
 
 print('Data choice made.')
