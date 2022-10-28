4ecb74f167cd20da8049b3b896d4c697672a7383
ValentinLAPLAUD <vallap.biophysics@protonmail.com>
2022-10-28 19:10:15 +0200

    ExperimentList update

--- ExperimentList.py
+++ ExperimentList.py
@@ -564,18 +564,18 @@ StackList221025_Ct_Osmo2 = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG6','PPG7','PP
 
 
 
-StackList221025_100mM = ['PPG1','PPG2','PPG3','PPG4','PPG5','PPG7','PPG8','PPG9','PPG10','PPG12','PPG13','PPG14','PPG15','PPG16',
-                        'PPG17','PPG18','PPG19','PPG20','PPG21','PPG22','PPG23','PPG24','PPG25','PPG26','PPG27','PPG28','PPG29','PPG30','PPG31','PPG32',
-                        'PPG33','PPG34','PPG35','PPG37','PPG38','PPG39','PPG40','PPG41','PPG42','PPG43','PPG44','PPG45','PPG46']
+StackList221025_100mM = ['PPG1','PPG2','PPG3','PPG5','PPG7','PPG8','PPG9','PPG10','PPG12','PPG13','PPG14','PPG15','PPG16',
+                        'PPG17','PPG18','PPG19','PPG20','PPG21','PPG22','PPG23','PPG24','PPG25','PPG26','PPG27','PPG29','PPG30','PPG31','PPG32',
+                        'PPG33','PPG34','PPG35','PPG37','PPG38','PPG39','PPG40','PPG42','PPG43','PPG44','PPG45','PPG46']
 
-StackList221025_100mM_Osmo = ['PPG2','PPG3','PPG5','PPG6','PPG7','PPG8','PPG9','PPG10','PPG12','PPG14','PPG15','PPG16',
+StackList221025_100mM_Osmo = ['PPG2','PPG3','PPG5','PPG7','PPG8','PPG9','PPG10','PPG12','PPG15','PPG16',
                             'PPG19','PPG20','PPG21','PPG22','PPG23','PPG25','PPG27','PPG28','PPG29','PPG31','PPG32','PPG33','PPG34','PPG35',
-                            'PPG38','PPG39','PPG41','PPG42','PPG43','PPG45','PPG46'
+                            'PPG38','PPG39','PPG42','PPG43','PPG45','PPG46',
                             'PPGA','PPGB','PPGC','PPGD','PPGE','PPGF','PPGH','PPGI','PPGJ']
 
-StackList221025_100mM_Osmo2 = ['PPG2','PPG3','PPG5','PPG6','PPG7','PPG8','PPG9','PPG12','PPG14','PPG15','PPG16',
+StackList221025_100mM_Osmo2 = ['PPG2','PPG3','PPG5','PPG7','PPG8','PPG9','PPG12','PPG14','PPG15','PPG16',
                             'PPG19','PPG20','PPG21','PPG22','PPG23','PPG25','PPG27','PPG28','PPG29','PPG31','PPG32','PPG33','PPG35',
-                            'PPG38','PPG39','PPG42','PPG43','PPG45','PPG46'
+                            'PPG38','PPG39','PPG42','PPG45','PPG46',
                             'PPGA','PPGB','PPGC','PPGD','PPGE','PPGF','PPGH','PPGI','PPGJ','PPGK']
 
 print('Data choice made.')
