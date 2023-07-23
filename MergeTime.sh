d4ae7f639abf7d066707dc5c1e81d274b7d371b5
Elise Muller <elisegmuller@orange.fr>
2023-07-23 16:49:56 +0200

    BashChanges

--- MergeTime.sh
+++ MergeTime.sh
@@ -7,49 +7,45 @@
 
 #destination_path=221128_choc_osmotique/choc-t15min/All/500mM/
 
-tile_nb=36
+tile_nb=54
 nb_deb=1
+#nb_deb_t5=19
 
-dir_path_t0=/run/media/emuller/TRANSCEND/221128_choc_osmotique/t0/
-#dir_path_t10=/run/media/emuller/TRANSCEND/221128_choc_osmotique/croissance_t23h30-t30h/
-#dir_path_t20=/run/media/emuller/Elise_these/Chip/$t20_path
 
-dir_path_destination=/run/media/emuller/TRANSCEND/221128_choc_osmotique/croissance/
+dir_path_t5=/run/media/emuller/TRANSCEND/Data/Chip/230524_storedgrowth_EF1aPME113.1/block3/
+#dir_path_t3=/run/media/emuller/TRANSCEND/Data/Chip/230321_mri_tak_chocs/croissance/tak/
+#dir_path_t4=/run/media/emuller/TRANSCEND/Data/Chip/230224_PI0_takmri/mriGb/t7h/
+#dir_path_t5=/run/media/emuller/Elise_4T/Data/Chip/2023/230321_mri_tak_chocs/croissance/tak/
 
-#cd $dir_path_t0
-#file_of_interest=$(find . -name "*ORG*" -type f)
-#rm $file_of_interest
+dir_path_destination=/run/media/emuller/TRANSCEND/Data/Chip/230524_storedgrowth_EF1aPME113.1/block1/
 
-#cd $dir_path_t10
-#file_of_interest=$(find . -name "*ORG*" -type f)
-#rm $file_of_interest
-
-#cd $dir_path_t20
-#file_of_interest=$(find . -name "*ORG*" -type f)
-#rm $file_of_interest
-
-#cd $dir_path_destination
-#file_of_interest=$(find . -name "*ORG*" -type f)
-#rm $file_of_interest
 
 #cp -RT $dir_path_t0 $dir_path_destination
 #cp -r $dir_path_t0 $dir_path_destination
 
 for ((i=$nb_deb; i <$tile_nb+$nb_deb; i++))
 do
-    cd $dir_path_t0
-    file_of_interest=$(find $(cd ..; pwd) -name "*1068($i)*" -type f)
-    #echo $file_of_interest
     
-    #cd $dir_path_t20
-    #file_of_interest2=$(find $(cd ..; pwd) -name "*($i)*" -type f)
-    #echo $file_of_interest2
+    #cd $dir_path_t2
+    #file_of_interest2=$(find $(cd ..; pwd) -name "*1230($i)*t0*" -type f)
+    
+    #cd $dir_path_t3
+    #file_of_interest3=$(find $(cd ..; pwd) -name "*1228($i)*" -type f)
+    
+    #cd $dir_path_t4
+    #file_of_interest4=$(find $(cd ..; pwd) -name "*1184($i)*" -type f)
+    
+    cd $dir_path_t5
+    #ibis=$(($nb_deb_t5+$i-1))
+    echo $i
+    file_of_interest5=$(find $(cd ..; pwd) -name "*Block3($i)*" -type f)
     
     cd $dir_path_destination
     directory_of_interest=$(find . -name "*($i)*" -type d)
-    cp $file_of_interest $directory_of_interest
-    #mv $file_of_interest2 $directory_of_interest
-    
+    #cp $file_of_interest2 $directory_of_interest
+    #cp $file_of_interest3 $directory_of_interest
+    #cp $file_of_interest4 $directory_of_interest
+    cp $file_of_interest5 $directory_of_interest
     
 done
 
--- Stitching.sh
+++ Stitching.sh
@@ -5,16 +5,17 @@
 
 #experiment_path=221128_croissance_PME/PME/
 tile_nb=18
-nb_deb=19
+nb_deb=1
 
 
 #dir_path=/run/media/emuller/Elise_these/Chip/$experiment_path
-dir_path=/run/media/emuller/TRANSCEND/221207_croissance_EGTA/Ctrl/
+dir_path=/run/media/emuller/Elise_these/Chip/2023/230602_Pi0_tak_mri/mri/choc8h/
 
 # copy files to stitching directory
 cd $dir_path
+
 mkdir --verbose -p stitching
-files_list=$(find . -name '*t5c*' -type f)
+files_list=$(find . -name '*t005*' -type f)
 cp $files_list stitching
 
 # rename files
@@ -22,8 +23,8 @@ cd stitching
 for ((i=$nb_deb; i <=$tile_nb+$nb_deb; i++))
 do
 	echo $i
-	res=$(($i%18))
-	if [[ $res -eq 0 ]];then res=18; fi ;
+	res=$(($i%$tile_nb))
+	if [[ $res -eq 0 ]];then res=$tile_nb; fi ;
 	if [[ $res -lt 10 ]]; then res="0${res}"; fi;
 	file_of_interest=$(find . -name "*($i)*" -type f)
 	echo $file_of_interest
