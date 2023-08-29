da20263d33b7e7f7d0c7a70f700619ee5dfa8156
Elise Muller <elisegmuller@orange.fr>
2023-08-29 11:16:36 +0200

    Stitching improvement

--- Stitching.sh
+++ Stitching.sh
@@ -4,18 +4,18 @@
 # to execute file : ./Stitching.sh
 
 #experiment_path=221128_croissance_PME/PME/
-tile_nb=18
-nb_deb=1
+tile_nb=15
+nb_deb=31
 
 
 #dir_path=/run/media/emuller/Elise_these/Chip/$experiment_path
-dir_path=/run/media/emuller/Elise_these/Chip/2023/230602_Pi0_tak_mri/mri/choc8h/
+dir_path=/run/media/emuller/Elise_these/Chip/2023/230617_croissance_EF1_fer5/fer5/
 
 # copy files to stitching directory
 cd $dir_path
 
 mkdir --verbose -p stitching
-files_list=$(find . -name '*t005*' -type f)
+files_list=$(find . -name '*t05*' -type f)
 cp $files_list stitching
 
 # rename files
@@ -23,7 +23,7 @@ cd stitching
 for ((i=$nb_deb; i <=$tile_nb+$nb_deb; i++))
 do
 	echo $i
-	res=$(($i%$tile_nb))
+	res=$((($i+1-$nb_deb)%$tile_nb))
 	if [[ $res -eq 0 ]];then res=$tile_nb; fi ;
 	if [[ $res -lt 10 ]]; then res="0${res}"; fi;
 	file_of_interest=$(find . -name "*($i)*" -type f)
